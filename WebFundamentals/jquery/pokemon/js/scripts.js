31567504fb36512411e538cda7883c99203feba9
Ze-Marcos <gringojaimes20@gmail.com>
2018-04-27 16:40:46 -0700

    pokemon homework assignment

--- WebFundamentals/jquery/pokemon/js/scripts.js
+++ WebFundamentals/jquery/pokemon/js/scripts.js
@@ -1,10 +1,10 @@
 
+var makePoke = (id) => '<img src="https://pokeapi.co/media/img/'+id+'.png">'
 
-
+var makeStaticPokemon  = () => [...Array(152).keys()].slice(1)
+                               .reduce((c,i) => c + makePoke(i) ,'')
 $(document).ready(function(){
-    console.log('I"mtire')
-    $('<div class"cont"></div>').appendTo('body')
-        var url = 'https://pokeapi.co/api/v2/pokemon/'+1+'/'
-        var image = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
-        $('<img src="">').attr('src','http://pokeapi.co/media/img/1.png').appendTo('.cont')
+    $('.images').html(makeStaticPokemon()).css({
+        'display':'inline-block',    
+    })
 })
