d43e3b9e5c5e3f9efa79839a7219c5877f095c19
Ze-Marcos <gringojaimes20@gmail.com>
2018-04-28 14:35:50 -0700

    changing weather-api-key

--- WebFundamentals/jquery/my-weather-api/js/scripts.js
+++ WebFundamentals/jquery/my-weather-api/js/scripts.js
@@ -5,7 +5,7 @@ var toFarenheight = (temp) => Math.trunc(temp*(9/5)-459.67)
 function makeHtml(city){
     var str = ''
     $.get('http://api.openweathermap.org/data/2.5/weather?q='+city+
-    ',us&&appid=1257ca04186493c1b0edc5d7ce94acbf',function(res){
+    ',us&&appid=(API-KEY-HERE)',function(res){
         str += '<h1>'+res.name+'</h1>'+
                 '<h3> Tempature: '+toFarenheight(res.main.temp)+'</h3>'
         $('.info').html(str).css({
