5c2ab073123155af024fe623afcf6303b97ee5d7
Ze-Marcos <gringojaimes20@gmail.com>
2018-05-11 11:01:02 -0700

    changes

---
+++ Python/django/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\my_files\courses\coding-dojo-homework\coding-dojo-assignments\python\django\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
