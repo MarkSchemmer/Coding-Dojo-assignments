7105096e12d7b06357380657f3b544785d5f0d79
Ze-Marcos <gringojaimes20@gmail.com>
2018-06-03 15:56:02 -0700

    changes for firstSharp and language_fundamentals

---
+++ netCore/firstSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace firstSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Enumerable.Range(0,20).ToList().ForEach(x => {
+                Console.WriteLine(x);
+            });
+        }
+    }
+}
--- netCore/language_fundamentals/basic_13/Program.cs
+++ netCore/language_fundamentals/basic_13/Program.cs
@@ -5,8 +5,6 @@ namespace basic_13
 {
     class Program
     {
-
-
         static void print_1_255() => Enumerable.Range(1,255).ToList().ForEach(x => Console.WriteLine(x));
         static void print_odd_1_255() => Enumerable.Range(1,255).Where(x => x%2!=0).ToList().ForEach(x => Console.WriteLine(x));
         static void PrintSum(int num) => Console.WriteLine(Enumerable.Range(1,num).Aggregate((acc, cur) => acc + cur, 0));
@@ -18,10 +16,6 @@ namespace basic_13
         static int[] elimNeg(int[] array) => array.Select(x => x > 0 ? x : 0).ToArray();
         static int[] shift(int[] array) => array.Skip(1).ToList().Add(0).ToArray();
         static string[] numberToString(int[] array) => array.Select(x => x > 0 ? x.ToString() : 'Dojo').ToArray();
-
-
-
-
 
         static void Main(string[] args)
         {
