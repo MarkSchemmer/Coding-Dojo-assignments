101869326f68d9b57e0aa6ad48577732be283945
Ze-Marcos <gringojaimes20@gmail.com>
2018-06-03 12:37:54 -0700

    multiplication_table

---
+++ netCore/language_fundamentals/multiplication_table/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+namespace multiplication_table
+{
+    class Program
+    {
+
+        static int[][] multTable()
+        {
+            return Enumerable.Range(1,10).Select(x => {
+                return Enumerable.Range(1,10).Select(z => z*x).ToArray();
+            }).ToArray();
+        }
+
+
+        static void Main(string[] args)
+        {
+            foreach (var item in multTable())
+            {
+                Console.WriteLine("[{0}]", String.Join(", ", item));
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
