c46857fe93df206f74635d90ee50012114c8437a
Ze-Marcos <gringojaimes20@gmail.com>
2018-06-04 14:24:35 -0700

    deck_of_cards

---
+++ netCore/oop/deck_of_cards/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace deck_of_cards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var deck = new Deck();
+            var mark = new Player("Mark Schemmer", deck);
+            var josh = new Player("Josh new guy", deck);
+
+            mark.displayDeck();
+            System.Console.WriteLine( "ceck to see if the same ----------------");
+            josh.displayDeck();
+            mark.draw();
+            josh.draw();
+            System.Console.WriteLine("cekccccc");
+            mark.displayDeck();
+            System.Console.WriteLine("-------------------- Hand - --------------------");
+            mark.displayHand();
+            System.Console.WriteLine("-------------------------------");
+            josh.displayDeck();
+            System.Console.WriteLine("------- Hand  ---------------------");
+            josh.displayHand();
+        }
+    }
+}
