3e3def290272cde76407c1272d7aea9eccaa5cb9
Ze-Marcos <gringojaimes20@gmail.com>
2018-06-05 00:51:07 -0700

    doulbeLinkedList

---
+++ netCore/oop/doulbeLinkedList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace doulbeLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new LinkedList<int>();
+            list.Add(1).Add(3).Add(5).Add(7).RemoveFront().RemoveBack().iter();
+            bool x = list.Contains(5, out int test);
+            if(x){
+                System.Console.WriteLine("Yeah it works!");
+                System.Console.WriteLine(test);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ netCore/oop/doulbeLinkedList/list.cs
@@ -0,0 +1,112 @@
+using System;
+
+namespace doulbeLinkedList
+{
+
+    public class Node<T>
+    {
+        public T value { get; set; }
+        public Node<T> Next { get; set; }
+        public Node<T> Prev { get; set; }
+        public Node(T value)
+        {
+            this.value = value;
+        }
+    }
+   public class LinkedList<T>
+   {
+       public int Count = 0;
+
+       public Node<T> Head { get; set; }
+       public Node<T> Tail { get; set; }
+
+       public LinkedList<T> Add(T value)
+       {
+            return AddToFront(new Node<T>(value));
+       } 
+       private LinkedList<T> AddToFront(Node<T> node)
+       {
+           this.Count++;
+
+           if(this.Head == null){
+               this.Head = node;
+               this.Tail = node;
+           }else{
+               var temp = this.Head;
+               this.Head = node;
+               this.Head.Next = temp;
+               temp.Prev = this.Head;
+           }
+
+           return this;
+       }
+
+       public LinkedList<T> RemoveFront()
+       {
+         if(this.Count > 0)
+         {
+             if(this.Count == 1)
+             {
+                 Clear();
+             }else{
+                 this.Head = this.Head.Next;
+                 this.Head.Prev = null;
+                 this.Count--;
+             }
+    
+         }
+         return this;
+       }
+
+       public LinkedList<T> RemoveBack()
+       {
+           if(this.Count > 0)
+           {
+               if(this.Count==1)
+               {
+                   Clear();
+               }else{
+                   this.Tail = this.Tail.Prev;
+                   this.Tail.Next = null;
+                   this.Count--;
+               }
+           }
+
+           return this;
+       }
+
+
+       public bool Contains(T _value, out T val)
+       {
+           var cur = this.Head;
+           while(cur!=null){
+               T dem = cur.value;
+                if(dem.Equals(_value)){
+                    val = cur.value;
+                    return true;
+                }   
+           }
+           val = default(T);
+           return false; 
+       }
+
+       public void Clear()
+       {
+           this.Head = null;
+           this.Tail = null;
+           this.Count = 0;
+       }
+       public LinkedList<T> iter()
+       {
+           var cur = this.Head;
+           while(cur!=null)
+           {
+            System.Console.WriteLine(cur.value);
+            cur = cur.Next;
+           }
+    
+            return this;
+       }
+
+   }
+}
