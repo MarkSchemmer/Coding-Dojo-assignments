69988bfecc6baeda4f052d72ab595dae2ed76e86
Ze-Marcos <gringojaimes20@gmail.com>
2018-06-07 13:40:36 -0700

    dojo_survey

---
+++ netCore/asp_net_core_1/dojo_survey/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using dojo_survey.Models;
+
+namespace dojo_survey.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        [Route("form")]
+        public IActionResult form(string name, string loc, string lan, string comment)
+        {
+            ViewBag.name = name;
+            ViewBag.loc = loc;
+            ViewBag.lan = lan;
+            ViewBag.comment = comment;
+            return View("Contact");
+        }
+    }
+}
